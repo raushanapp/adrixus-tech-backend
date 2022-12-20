ac996a97329b0827e7719f8e56f3523026498b0a
raushan kuamr <sraushan890@gmail.com>
2022-12-21 01:19:12 +0530

    src folder shift inside of server js

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,39 +1,41 @@
-const User = require("../model/userModel");
+const express = require("express");
+const router = express.Router();
 
-const user = async (req, res) => {
-    const { first_name,sort,page,limit } = req.query;
+const User = require("../model/userModel");
+router.get("", async (req, res) => {
+    const { first_name, sort, page, limit } = req.query;
     const queryObject = {};
 
     if (first_name) {
         queryObject.first_name = first_name;
         console.log(queryObject)
     }
-    let apiData= User.find(queryObject)
+    let apiData = User.find(queryObject)
     if (sort) {
         let sortFix = sort.replace(",", " ");
         apiData = apiData.sort(sortFix);
-      //    console.log(sort)
+        //    console.log(sort)
     }
     // pagination 
     if (page && limit) {
-        queryObject.page=page
-        queryObject.limit=limit
+        queryObject.page = page
+        queryObject.limit = limit
     }
-    let page_no=Number(queryObject.page)||1;
-    let total_limit=Number(queryObject.limit)||10;
+    let page_no = Number(queryObject.page) || 1;
+    let total_limit = Number(queryObject.limit) || 10;
     let skip = (page_no - 1) * total_limit;
     apiData = apiData.skip(skip).limit(total_limit);
     console.log(queryObject)
 
     try {
         const myUser = await apiData;
-        return res.status(200).json({users:myUser,total:myUser.length})
+        return res.status(200).json({ users: myUser, total: myUser.length })
 
         
     } catch (err) {
         return res.status(400).send(err);
     }
-}
+});
 
-module.exports = user;
+module.exports = router;
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get("/users", userController);
+app.use("/users", userController);
 app.post("/register",register);
 app.post("/login",login);
 module.exports = app;
