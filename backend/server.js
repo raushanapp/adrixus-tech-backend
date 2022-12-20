fce59f005a4b047947f66b1256b7cdf345da3f47
raushan kuamr <sraushan890@gmail.com>
2022-12-20 23:58:50 +0530

    pakage json main  name change

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const app = require("./index")
-const connect = require("./config/db");
+const app = require("./src/index")
+const connect = require("./src/config/db");
 
 const port = process.env.PORT || 2500;
 app.listen(port, async () => {
     try {
         await connect();
-        console.log("Listening on port 2500....")
+        console.log(`Listening on port ${port}`)
     } catch (err) {
         console.log(err);
     }
