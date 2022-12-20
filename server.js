367f07b2322f84e04b274e2ddf56e7910370309e
raushan kuamr <sraushan890@gmail.com>
2022-12-20 18:36:16 +0530

    pakage.json change or name change

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require("./app")
-const connect = require("./config/db");
+const app = require("./server/src/app")
+const connect = require("./server/src/config/db");
 
 const port = process.env.PORT || 2500;
 app.listen(port, async () => {
